fd5ce7a56c5c82b6f0662b1d14a14784d04792f7
LuisChen <leo5916267@gmail.com>
2019-04-19 16:43:50 +0800

    建好雛形

---
+++ src/layout/Footer/Footer.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const Footer = styled.div`
+	height: 200px;
+	background-color: #a6adb4;
+	box-sizing: border-box;
+	padding: 60px;
+`
+
+const FooterInput = styled.input`
+	border-radius: 3px;
+	border-style: solid;
+	border-color: #ebeded;
+	background-color: #a6adb4;
+	height: 20px;
+	width: 260px;
+`
+
+export { Footer, FooterInput }
