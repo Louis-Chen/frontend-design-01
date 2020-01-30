7fb78748bc3709478ff1ebefc14d0843dc67bcc4
LuisChen <leo5916267@gmail.com>
2020-01-30 16:58:29 +0800

    add nav layout

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react'
 
 import { injectGlobal } from 'emotion'
 import Helvetica from './assets/fonts/helvetica.woff'
+import HelveticaBold from './assets/fonts/helvetica-bold.woff'
 import Nav from './components/Nav'
 
 injectGlobal`
-    margin:0 auto;
+    body{
+        margin:0 auto;
+    }
+
     * {
         box-sizing: border-box;
-
     }
     @font-face {
         font-family: "Helvetica";
         src: url(${Helvetica});
+        font-weight:normal;
     }
+    @font-face {
+        font-family: "Helvetica";
+        src: url(${HelveticaBold});
+        font-weight: bold;
+    }   
     font-family: "Helvetica";
 
 `
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,21 @@
 import React from 'react'
-
+import S from './Nav.emotion'
 const Nav = () => {
-	return <div>Nav</div>
+	return (
+		<S.Nav>
+			<S.Bar>
+				<S.LeftBar>LOGO</S.LeftBar>
+
+				<S.RightBar>
+					<S.Item>HOME</S.Item>
+					<S.Item>DISCOVERY</S.Item>
+					<S.Item>PHOTOS</S.Item>
+					<S.Item>CONTACT</S.Item>
+					<S.Avatar></S.Avatar>
+				</S.RightBar>
+			</S.Bar>
+		</S.Nav>
+	)
 }
 
 export default Nav
