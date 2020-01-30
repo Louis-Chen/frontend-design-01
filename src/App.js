15c8bf750480e239d215fa83c2bffad1b37ac12a
LuisChen <leo5916267@gmail.com>
2020-01-30 15:49:34 +0800

    remove old code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,8 @@
 import React from 'react'
 
-import { ThemeProvider } from 'styled-components'
-
-import { createGlobalStyle } from 'styled-components'
-
-import PageLanding from './pages/Landing'
-import Navagation from './layout/Nav/Menu'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-	margin:auto;
-  }
-`
-
-const theme = {
-	flexboxgrid: {
-		// Defaults
-		gridSize: 12, // columns
-		gutterWidth: 4, // rem
-		outerMargin: 2, // rem
-		mediaQuery: 'only screen',
-		container: {
-			sm: 46, // rem
-			md: 61, // rem
-			lg: 76 // rem
-		},
-		breakpoints: {
-			xs: 0, // em
-			sm: 48, // em
-			md: 64, // em
-			lg: 75 // em
-		}
-	}
-}
 const App = () => {
 	return (
-		<ThemeProvider theme={theme}>
-			<React.Fragment>
-				<GlobalStyle />
-				<Navagation />
-				<PageLanding />
-			</React.Fragment>
-		</ThemeProvider>
+		<div>2020</div>
 	)
 }
 
