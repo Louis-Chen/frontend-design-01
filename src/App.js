fb94cf2ac5c474397d9e2a388fe5454dfbc44912
LuisChen <leo5916267@gmail.com>
2020-01-30 16:15:53 +0800

    add global

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import { injectGlobal } from 'emotion'
+import Helvetica from './assets/fonts/helvetica.woff'
+import Nav from './components/Nav'
+
+injectGlobal`
+    margin:0 auto;
+    * {
+        box-sizing: border-box;
+
+    }
+    @font-face {
+        font-family: "Helvetica";
+        src: url(${Helvetica});
+    }
+    font-family: "Helvetica";
+
+`
 const App = () => {
 	return (
-		<div>2020</div>
+		<div>
+			<Nav></Nav>
+		</div>
 	)
 }
 
